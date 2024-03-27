4e7f81772abc831817d5395ecca398ac8b5b6c57
JakubDob <devjakubd@gmail.com>
2024-03-27 21:47:49 +0100

    Added default config options for canvas grid

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  CanvasGridDefaultOptions,
+  CANVAS_GRID_DEFAULT_OPTIONS,
+} from '@jakubdob/ngx-canvas-grid';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    {
+      provide: CANVAS_GRID_DEFAULT_OPTIONS,
+      useValue: <CanvasGridDefaultOptions>{
+        cols: 9,
+        rows: 9,
+        spacing: 1,
+        cellWidth: 40,
+        cellHeight: 40,
+      },
+    },
+  ],
 };
