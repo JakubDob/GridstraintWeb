9e0bdb0786f7f2706f28e966566b01426fd3e64e
JakubDob <devjakubd@gmail.com>
2024-03-27 21:53:45 +0100

    Set main app route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    loadComponent: () =>
+      import(
+        './components/solver/solver-manager/solver-manager.component'
+      ).then((module) => module.SolverManagerComponent),
+  },
+];
