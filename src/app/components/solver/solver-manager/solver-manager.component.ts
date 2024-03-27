5c63d6bfe113acf75d2d4014d85ae543bdd38af6
JakubDob <devjakubd@gmail.com>
2024-03-27 21:52:35 +0100

    Added solver manager

---
+++ src/app/components/solver/solver-manager/solver-manager.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { SolverConstraintManagerComponent } from '../solver-constraint-manager/solver-constraint-manager.component';
+import { SolverGridActionsComponent } from '../solver-grid-actions/solver-grid-actions.component';
+import { SolverGridComponent } from '../solver-grid/solver-grid.component';
+import { SolverSolutionOptionsComponent } from '../solver-solution-options/solver-solution-options.component';
+import { SolverValueOptionsComponent } from '../solver-value-options/solver-value-options.component';
+
+@Component({
+  selector: 'app-solver-manager',
+  standalone: true,
+  imports: [
+    SolverGridComponent,
+    SolverGridActionsComponent,
+    SolverValueOptionsComponent,
+    SolverSolutionOptionsComponent,
+    SolverConstraintManagerComponent,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+  ],
+  templateUrl: './solver-manager.component.html',
+  styleUrl: './solver-manager.component.scss',
+})
+export class SolverManagerComponent {}
