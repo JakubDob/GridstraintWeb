a5a6d7d5b7d3a56e2aa2f5735d91ec941b167f45
JakubDob <devjakubd@gmail.com>
2024-03-27 21:49:20 +0100

    Expanded solver types

--- src/app/types/solver-types.ts
+++ src/app/types/solver-types.ts
@@ -1,19 +1,51 @@
+export interface SolverConstraint {}
+
+export type SolverConstraintDetails = {
+  constraint: new () => SolverConstraint;
+  toSolverCode(
+    cells: ReadonlySet<CellIndex>,
+    settings?: ReadonlyMap<string, string>
+  ): string;
+  hasSettings: boolean;
+};
+
+export enum RelationSymbol {
+  LESS_THAN = '<',
+  LESS_OR_EQUAL = '<=',
+  EQUAL = '=',
+  GREATER_THAN = '>',
+  GREATER_OR_EQUAL = '>=',
+}
+
+export enum SolvingMethod {
+  SATISFY = 'satisfy',
+  MINIMIZE = 'minimize',
+  MAXIMIZE = 'maximize',
+}
+
 export type GridConstraint = {
   readonly name: string;
   readonly views: GridView[];
 };
 
 export type GridView = {
-  name: string;
+  readonly name: string;
+  readonly parent: GridConstraint;
   readonly groups: CellGroup[];
-  readonly indexToCellGroup: Map<number, CellGroup[] | null>;
+  readonly indexToCellGroup: Map<number, CellGroup>;
+  readonly settings?: ReadonlyMap<string, string>;
 };
 
 export type CellGroup = {
-  name: string;
+  readonly name: string;
   backgroundColor: string;
   readonly indices: Set<number>;
   readonly parent: GridView;
+};
+
+export type CellGroupAndIndex = {
+  readonly group: CellGroup;
+  readonly index: number;
 };
 
 export type CellIndex = number;
@@ -26,4 +58,15 @@ export type GridSize = {
 export type ValueRange = {
   min: number;
   max: number;
+};
+
+export type ValueChange<T> = {
+  previous: T | null;
+  current: T | null;
+};
+
+export type IndexedValueChange<T> = {
+  index: number;
+  previous: T | null;
+  current: T | null;
 };
